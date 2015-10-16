3999b736b66f71b2e2357ee8327e3ef4e7d7e898
aytona <christopher.aytona@gmail.com>
2015-10-16 00:26:47 -0400

    Turret Defense - Player Boundary & Controller

---
+++ GAME3001_Aytona_Christopher_Proj_M2/Assets/Scripts/TODO.cs
@@ -0,0 +1,23 @@
+﻿/*
+
+Milestone 2: Basic Protype
+
+Level Design
+- Boundaries
+- Obstacles
+- Player
+- Camera Setup
+
+Gameplay
+- Player Weapon
+- Player Upgrade
+
+Enemy AI
+- One enemy behaviour
+- Enemy Weapon
+
+Scenes
+- Main Menu
+- Instructions
+
+*/
