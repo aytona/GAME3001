4b96ef9e7717ea043b6da0d90512a2ab8bad0f5a
aytona <christopher.aytona@gmail.com>
2015-10-25 22:14:09 -0400

    MenuUI.cs added

---
+++ GAME3001_Aytona_Christopher_Proj_M2/Assets/Scripts/MenuUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuUI : MonoBehaviour {
+
+    public GameObject HowTo;
+    public GameObject MainMenu;
+
+    public void doStart()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void doHowTo()
+    {
+        MainMenu.SetActive(false);
+        HowTo.SetActive(true);
+    }
+
+    public void doQuit()
+    {
+        Application.Quit();
+    }
+
+    public void doMenu()
+    {
+        MainMenu.SetActive(true);
+        HowTo.SetActive(false);
+    }
+}
