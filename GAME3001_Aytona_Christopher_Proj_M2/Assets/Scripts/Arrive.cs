1c9f81c6e2fbbe766a632ac35d2bc21a0fbcfe68
aytona <christopher.aytona@gmail.com>
2015-10-25 21:48:31 -0400

    Modify Arrive.cs to shoot when arrived
    TODO: Put shooting mechanic on a different script

--- GAME3001_Aytona_Christopher_Proj_M2/Assets/Scripts/Arrive.cs
+++ GAME3001_Aytona_Christopher_Proj_M2/Assets/Scripts/Arrive.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class Arrive : MonoBehaviour
 {
     public Vector3 targetPosition;
+    public GameObject enemyBullet;
+    public Transform bulletPosition;
+    public float rateOfFire = 3f;
 
     private Steering steering;
 
@@ -17,5 +20,25 @@ public class Arrive : MonoBehaviour
         Vector3 accel = steering.Arrive(targetPosition);
         steering.Steer(accel);
         steering.LookWhereYoureGoing();
+
+        if (targetPosition == transform.position)
+        {
+            StartCoroutine(ShootCoroutine(rateOfFire));
+        }
 	}
+
+    private IEnumerator ShootCoroutine(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+        }
+    }
+    
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(this.enemyBullet) as GameObject;
+        bullet.transform.position = bulletPosition.transform.position;
+        bullet.transform.rotation = bulletPosition.transform.rotation;
+    }
 }
