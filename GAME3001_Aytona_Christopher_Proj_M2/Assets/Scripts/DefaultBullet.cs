a7ce58820bbd5bed73929bd507a8539c73d5cfd7
aytona <christopher.aytona@gmail.com>
2015-10-25 21:27:54 -0400

    DefaultBullet.cs added

---
+++ GAME3001_Aytona_Christopher_Proj_M2/Assets/Scripts/DefaultBullet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class DefaultBullet : MonoBehaviour {
+
+    public float speed = 5f;
+
+    private float lifeSpan = 10f;
+    // private bool isDestroyed = false;       // For particle system
+    private Vector3 myDirection = Vector3.right;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        StartCoroutine(LifespanCoroutine());
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(myDirection * speed);
+    }
+
+    private IEnumerator LifespanCoroutine()
+    {
+        yield return new WaitForSeconds(this.lifeSpan);
+        Destroy(this.gameObject);
+    }
+}
