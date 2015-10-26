9b568c76a505201e38fc2bc629fee06072978f35
aytona <christopher.aytona@gmail.com>
2015-10-25 21:20:39 -0400

    Gun.cs added

--- GAME3001_Aytona_Christopher_Proj_M2/Assets/Scripts/Gun.cs
+++ GAME3001_Aytona_Christopher_Proj_M2/Assets/Scripts/Gun.cs
@@ -3,6 +3,15 @@ using System.Collections;
 
 public class Gun : MonoBehaviour {
 
-    public GameObject[] bulletPrefabs;
+    public GameObject defaultBullet;
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bullet = Instantiate(this.defaultBullet) as GameObject;
+            bullet.transform.position = this.transform.position;
+            bullet.transform.rotation = this.transform.rotation;
+        }
+    }
 }
